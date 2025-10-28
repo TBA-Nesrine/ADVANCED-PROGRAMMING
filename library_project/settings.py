538ac2836280c19180c36eeeaaed1a98e200a7fc
unknown <kamiri.lilia1952006@gmail.com>
2025-10-28 15:30:21 +0100

    postegresql -> sqllite

--- library_project/settings.py
+++ library_project/settings.py
@@ -72,7 +72,7 @@ WSGI_APPLICATION = 'library_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -81,6 +81,12 @@ DATABASES = {
         'PASSWORD': 'mypassword',
         'HOST': 'localhost',
         'PORT': '5432',
+    }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
